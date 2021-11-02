4b579d94143329526b661f326ac9503a0e043c3c
Nacorga <ncg2392@gmail.com>
2021-11-02 14:30:35 +0100

    config: app name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/google-maps'),
+      dir: require('path').join(__dirname, './coverage/gm-places-map'),
       subdir: '.',
       reporters: [
         { type: 'html' },
